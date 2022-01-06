3b6ef06adf5881c46c84d6b9ffc0b3b2d709f0c4
SharkiCS <Sharkismanagements@gmail.com>
2022-01-07 00:23:57 +0100

    feat: ShowReturnsAStringWithAllComands

---
+++ TelegramShell.Tests/ListOfCommandsTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace TelegramShell.Tests
+{
+    public class ListOfCommandsTest
+    {
+        [Fact]
+        public void ShowReturnsAStringWithAllComands()
+        {
+            ListOfCommands commands = new ListOfCommands();
+            commands.Show().Should().Be(
+                "/Show\n/Cmd\n"
+                );
+        }
+    }
+}
--- TelegramShell/ListOfCommands.cs
+++ TelegramShell/ListOfCommands.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace TelegramShell
 {
-    class ListOfCommands
+    public class ListOfCommands
     {
         public string Show()
         {
